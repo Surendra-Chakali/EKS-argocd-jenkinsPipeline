aaa4e27a8a1b2ce52bf19c8d6fe3981bdba58a49
root <root@ip-10-0-24-77.eu-west-2.compute.internal>
2023-12-28 14:03:24 +0000

    created java project with maven

---
+++ suri-app/src/main/java/com/surendra/app/App.java
@@ -0,0 +1,13 @@
+package com.surendra.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Surendra! Congrats" );
+    }
+}
